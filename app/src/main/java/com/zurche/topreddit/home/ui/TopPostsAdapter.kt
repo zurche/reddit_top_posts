802acb5d24c6639a14a41bb775a8204752f13e25
Alejandro Zurcher <alejandro.zurcher@gmail.com>
2020-10-07 11:06:36 +0100

    Updating Adapter logic

--- app/src/main/java/com/zurche/topreddit/home/ui/TopPostsAdapter.kt
+++ app/src/main/java/com/zurche/topreddit/home/ui/TopPostsAdapter.kt
@@ -1,18 +1,14 @@
 package com.zurche.topreddit.home.ui
 
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.URLUtil
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import com.zurche.topreddit.R
 import com.zurche.topreddit.home.data.remote.service.ChildrenData
+import com.zurche.topreddit.home.data.remote.service.NewsData
 import kotlinx.android.synthetic.main.post_item.view.*
 import java.util.*
 import java.util.concurrent.TimeUnit
@@ -26,31 +22,16 @@ class TopPostsAdapter : RecyclerView.Adapter<TopPostsAdapter.TopPostViewHolder>(
 
     private val topPosts: MutableList<ChildrenData> = ArrayList()
 
-    private var context: Context? = null
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopPostViewHolder {
         val itemView = LayoutInflater
                 .from(parent.context)
                 .inflate(R.layout.post_item, parent, false)
-        context = parent.context
         return TopPostViewHolder(itemView)
     }
 
-    override fun getItemCount(): Int {
-        return topPosts.size
-    }
+    override fun getItemCount() = topPosts.size
 
-    override fun onBindViewHolder(holder: TopPostViewHolder, position: Int) {
-        holder.title.text = topPosts[position].data.title
-        val byAuthorString = context!!.getString(R.string.by, topPosts[position].data.author)
-        holder.author.text = byAuthorString
-        val commentsCountString = context!!.getString(R.string.comments, topPosts[position].data.num_comments)
-        holder.commentsAmount.text = commentsCountString
-        val postCreatingString = context!!.getString(R.string.created, getFormatedDate(position))
-        holder.entryDate.text = postCreatingString
-        val thumbnailImageUrl = topPosts[position].data.thumbnail
-        setupThumbnail(holder, thumbnailImageUrl)
-    }
+    override fun onBindViewHolder(holder: TopPostViewHolder, position: Int) = holder.bind(topPosts[position].data)
 
     /**
      * Update the posts list.
@@ -60,41 +41,38 @@ class TopPostsAdapter : RecyclerView.Adapter<TopPostsAdapter.TopPostViewHolder>(
         topPosts.addAll(updatedPosts)
     }
 
-    /**
-     * Process the UNIX timestamp of the post creating date and return a format like "x hours"
-     *
-     * @param position current post element position being checked
-     * @return hours since its creation
-     */
-    private fun getFormatedDate(position: Int): Long {
-        val postDate = Date(topPosts[position].data.created * 1000)
-        val elapsedTime = Date().time - postDate.time
-        return TimeUnit.MILLISECONDS.toHours(elapsedTime)
-    }
-
-    private fun setupThumbnail(holder: TopPostViewHolder, thumbnailImageUrl: String) {
-        if (URLUtil.isValidUrl(thumbnailImageUrl)) {
-            holder.thumbnail.visibility = View.VISIBLE
-            Picasso.get()
-                    .load(thumbnailImageUrl)
-                    .resize(100, 100)
-                    .centerCrop()
-                    .into(holder.thumbnail)
-            holder.thumbnail.setOnClickListener {
-                val i = Intent(Intent.ACTION_VIEW)
-                i.data = Uri.parse(thumbnailImageUrl)
-                context!!.startActivity(i)
-            }
-        } else {
-            holder.thumbnail.visibility = View.GONE
-        }
-    }
-
     inner class TopPostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var title: TextView = itemView.title
-        var author: TextView = itemView.author
-        var entryDate: TextView = itemView.entry_date
-        var commentsAmount: TextView = itemView.comments_amount
-        var thumbnail: ImageView = itemView.thumbnail
+
+        fun bind(newsData: NewsData) {
+            itemView.apply {
+                title.text = newsData.title
+                author.text = context.getString(R.string.by, newsData.author)
+                comments_amount.text = context.getString(R.string.comments, newsData.num_comments)
+                entry_date.text = context.getString(R.string.created, getFormatedDate(newsData))
+
+                if (URLUtil.isValidUrl(newsData.thumbnail)) {
+                    thumbnail.visibility = View.VISIBLE
+                    Picasso.get()
+                            .load(newsData.thumbnail)
+                            .resize(100, 100)
+                            .centerCrop()
+                            .into(thumbnail)
+                } else {
+                    thumbnail.visibility = View.GONE
+                }
+            }
+        }
+
+        /**
+         * Process the UNIX timestamp of the post creating date and return a format like "x hours"
+         *
+         * @param newsData current post element position being checked
+         * @return hours since its creation
+         */
+        private fun getFormatedDate(newsData: NewsData): Long {
+            val postDate = Date(newsData.created * 1000)
+            val elapsedTime = Date().time - postDate.time
+            return TimeUnit.MILLISECONDS.toHours(elapsedTime)
+        }
     }
 }
