bf6957b6a0838d510ff3584371424cdca0d77ab2
Alejandro Zurcher <alejandro.zurcher@gmail.com>
2020-10-08 20:50:35 +0100

    Migrating PostListFragment to use binding

--- app/src/main/java/com/zurche/topreddit/home/ui/PostListFragment.kt
+++ app/src/main/java/com/zurche/topreddit/home/ui/PostListFragment.kt
@@ -8,7 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.zurche.topreddit.R
+import com.zurche.topreddit.databinding.PostListFragmentBinding
 import com.zurche.topreddit.home.data.remote.TopPostRemoteDataSource
 import com.zurche.topreddit.home.list.TopPostLoadStateAdapter
 import com.zurche.topreddit.home.list.TopPostsAdapter
@@ -19,13 +19,15 @@ import kotlinx.coroutines.launch
 
 class PostListFragment : Fragment() {
 
+    private lateinit var binding: PostListFragmentBinding
     private lateinit var viewModel: PostListViewModel
     private lateinit var adapter: TopPostsAdapter
     private var loadTopPostsJob: Job? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.post_list_fragment, container, false)
+        binding = PostListFragmentBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
